fc7b29fcc0ae6ad77d3d1ce6b147b5d61a0be0b4
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-01-29 16:37:23 +0330

    Update views.py
    برای کارکردن توابع مربوط به متود post استفاده از csrf_exempt فراموش نشود

--- library/views.py
+++ library/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from library.forms import SignUpForm
+
 
 # Create your views here.
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(
+                'User created successfully!'
+            )
+        return HttpResponse(f"{form.errors}")
+    return HttpResponse('Only post method allowed!')
