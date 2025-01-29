cf5ac3914c8512a54baacc97a664f3f1361e80a9
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-01-29 18:29:28 +0330

    register -view-

--- library/views.py
+++ library/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from library.forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 @csrf_exempt
@@ -21,3 +22,18 @@ def signup(request):
 def user_list(request):
     users = User.objects.all()
     return render(request, 'user_list.html', {'users': users})
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if not form.is_valid():
+            return render(request, 'register.html', {'form': form})
+        form.save()
+        return HttpResponse('User Created!')
+
+    elif request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+    return HttpResponse('Method NOT allowed!')
