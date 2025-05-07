3a644d7537c9dd9b3059e5539a77aef19facb0e0
moali1010 <moali.develop@gmail.com>
2025-05-07 11:06:25 +0330

    migrations operations

--- library/models.py
+++ library/models.py
@@ -38,7 +38,49 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 # class CustomUser(AbstractUser):
 #     email = models.EmailField(unique=True)
 #     country = models.CharField(max_length=100)
 #     phone_number = models.CharField(max_length=50, blank=True, null=True)
+
+
+# class Cake(models.Model):
+#     sauce = models.CharField(default="Chocolate", max_length=20)
+
+"""
+from django.db import migrations
+from django.db.models import CharField, Value, F
+from django.db.models.functions import Concat, Replace
+
+
+def alter_names_of_sauces(apps, schema_editor):
+    Cake = apps.get_model('app', 'Cake')
+
+    Cake.objects.annotate(
+        new_sauce_name=Concat(
+            F('sauce'), Value(' Sauce'), output_field=CharField()
+        )
+    ).update(sauce=F('new_sauce_name'))
+
+
+def reverse_alter_names_of_sauces(apps, schema_editor):
+    Cake = apps.get_model('app', 'Cake')
+
+    Cake.objects.update(
+        sauce=Replace('sauce', text=Value(' Sauce'), replacement=Value(''))
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            alter_names_of_sauces,
+            reverse_code=reverse_alter_names_of_sauces
+        ),
+    ]
+"""
